ba6cf6ec8adbf4d30f5ba8a4f7039f0396e2d156
kurkmas <kurkmas@inbox.ru>
2021-04-09 00:43:44 +0300

    tabs function - done

--- Pulse/js/script.js
+++ Pulse/js/script.js
@@ -9,3 +9,35 @@ const swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   }
 });
+
+const choiseTab = document.querySelectorAll('.choise');
+const tabsLink = document.querySelectorAll('.tabs__link');
+const tabsHiddenLink = document.querySelectorAll('.tabs__hidden-link');
+
+choiseTab.forEach(function (item) {
+  item.addEventListener('click', function (event) {
+    let dataButton = event.currentTarget.dataset.button;
+    choiseTab.forEach(function (item) {
+      item.classList.remove('choise--active');
+    });
+    this.classList.add('choise--active');
+    document.querySelectorAll('.tabs__list').forEach(function (item) {
+      item.classList.remove('tabs__list--active');
+      document.querySelector(`[data-tabs="${dataButton}"]`).classList.add('tabs__list--active');
+    });
+  });
+});
+
+tabsLink.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+    this.parentElement.parentElement.querySelector('.tabs__hidden').classList.add('tabs__hidden--active');
+  });
+});
+
+tabsHiddenLink.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+    this.parentElement.classList.remove('tabs__hidden--active');
+  });
+});
