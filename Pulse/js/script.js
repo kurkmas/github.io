76fa9503453dfca3e2ec6157a8ffc23139903d1f
kurkmas <kurkmas@inbox.ru>
2021-04-08 19:27:21 +0300

    main layout - finished

---
+++ Pulse/js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const swiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
