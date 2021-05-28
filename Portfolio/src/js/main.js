4c846b469589b52306f02ca7f06fda8112167ef0
kurkmas <kurkmas@inbox.ru>
2021-05-28 21:06:47 +0300

    main layout - finished

--- Portfolio/src/js/main.js
+++ Portfolio/src/js/main.js
@@ -1,1 +1,13 @@
-console.log('hello from gulp file!');
+'use strict';
+
+const burger = document.querySelector('.burger'),
+  scale = document.querySelectorAll('.rating__line_orange'),
+  percent = document.querySelectorAll('.rating__percent');
+
+burger.addEventListener('click', function () {
+  burger.classList.toggle('burger_active');
+});
+
+percent.forEach(function (item, i) {
+  scale[i].style.width = item.innerHTML;
+});
