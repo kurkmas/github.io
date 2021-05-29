1dfd67d7c535163779885da1d4d146d459d3ebe9
kurkmas <kurkmas@inbox.ru>
2021-05-30 00:03:52 +0300

    portfolio - finished

--- Portfolio/src/js/main.js
+++ Portfolio/src/js/main.js
@@ -2,10 +2,18 @@
 
 const burger = document.querySelector('.burger'),
   scale = document.querySelectorAll('.rating__line_orange'),
-  percent = document.querySelectorAll('.rating__percent');
+  percent = document.querySelectorAll('.rating__percent'),
+  closeBtn = document.querySelector('.close__btn'),
+  header = document.querySelector('.header');
 
 burger.addEventListener('click', function () {
-  burger.classList.toggle('burger_active');
+  burger.classList.add('burger_active');
+  header.classList.add('header_active');
+});
+
+closeBtn.addEventListener('click', function () {
+  burger.classList.remove('burger_active');
+  header.classList.remove('header_active');
 });
 
 percent.forEach(function (item, i) {
