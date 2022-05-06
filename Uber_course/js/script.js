cdca915f7fea09b409e4706e1b7d5c3c60d6582e
Kurkmas Shokirov <kurkmas@inbox.ru>
2022-05-06 11:03:46 +0500

    restructure projects

--- Uber_course/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('.burger').on('click', function () {
-    $('.nav-list').addClass('nav-list--active');
-  });
-
-  $('.close').on('click', function () {
-    $('.nav-list').removeClass('nav-list--active');
-  });
-});
